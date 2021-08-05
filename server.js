d9c56e94507a3d0a3a3142a14217e2b36629a6ce
Mitul David <mituldavid@gmail.com>
2021-08-05 21:28:18 +0530

    Updated dependencies

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const rateLimit = require('express-rate-limit');
+const path = require('path');
 
 app = express();
 connectDB();
@@ -24,9 +25,14 @@ app.use('/api/reclist', require('./routes/api/reclist'));
 app.use('/api/search', require('./routes/api/search'));
 app.use('/api/pinnedlist', require('./routes/api/pinnedlist'));
 
-app.get('/', (req, res) => {
-  res.send('<h1>Test</h1>');
-});
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
